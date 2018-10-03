cb4519c625074d69b008517468564b1fd02dc2c7
k-nasa <htilcs1115@gmail.com>
2018-10-03 23:57:06 +0900

    Delete header component loading

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
 import Router from './components/router'
-import Header from './components/header'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './style/style.css'
 

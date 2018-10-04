51aaa9ee9392ec963ccb96c2b83134543dbd5564
k-nasa <htilcs1115@gmail.com>
2018-10-04 09:32:14 +0900

    Fix/ menu component style

--- src/components/menue.js
+++ src/components/menue.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import icon from './../assets/icon.jpg'
+import '../style/menu.css'
 
 export default () => (
   <div className='menu'>
@@ -9,28 +9,24 @@ export default () => (
         <div></div>
         <div></div>
       </Link>
-    <Link to='profile'>
-      <div class="card">
-        <img class="card-img-top" src={icon} alt="icon"/>
+      <div className='cards card-links'>
+        <Link to='profile' className='card'>
           <div class="card-body">
             <h5 class="card-title">Profile</h5>
             <p class="card-text">
-            RustとかDartを書いてる人。サーバーサイドエンジニア見習い。お仕事ではRails/Reactを書いてる
-            </p>
+              RustとかDartを書いてる人。サーバーサイドエンジニア見習い。お仕事ではRails/Reactを書いてる
+              </p>
           </div>
-        </div>
-    </Link>
-    <Link to='works'>
-      <div class="card">
-        <img class="card-img-top" src={icon} alt="icon"/>
+        </Link>
+        <Link to='works' className='card'>
           <div class="card-body">
             <h5 class="card-title">Works</h5>
             <p class="card-text">
-            今まで作った作品紹介
-            </p>
+              今まで作った作品紹介
+              </p>
           </div>
-        </div>
         </Link>
-      </nav>
+      </div>
+    </nav>
   </div>
 )
